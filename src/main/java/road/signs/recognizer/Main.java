dbdb13104d9eaa973282d4bc5bfcd9e7d322aba1
pko <kolanowskipiotr@gmail.com>
2016-05-11 15:17:09 +0200

    dodanie możliwości odczytu i zapisu sieci do pliku

--- src/main/java/road/signs/recognizer/Main.java
+++ src/main/java/road/signs/recognizer/Main.java
@@ -5,7 +5,13 @@ import org.encog.Encog;
 import org.encog.ml.data.MLData;
 import org.encog.ml.data.MLDataPair;
 import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
 import org.encog.neural.networks.BasicNetwork;
+import org.encog.persist.EncogPersistor;
+import org.encog.util.obj.SerializeObject;
+
+import java.io.File;
+import java.io.IOException;
 
 import static road.signs.recognizer.TrainingSetBuilder.*;
 
@@ -14,6 +20,8 @@ import static road.signs.recognizer.TrainingSetBuilder.*;
  */
 public class Main {
 
+
+    private static final String FILENAME = "encogexample.ser";
 
     public static void main(String[] args) {
 
@@ -26,11 +34,41 @@ public class Main {
         MLDataSet trainingSet = TrainingSetBuilder.buildTrainingSet();
         NetworkTrainer.train(network, trainingSet, NetworkTrainer.DEFAULT_ERROR_LEVEL);
 
-
         MLDataSet testingSet = TrainingSetBuilder.buildTestingSet();
         testingSet.forEach(queryData -> askNetwork(network, queryData));
 
+
+        persistNetwork(network, trainingSet);
+        loadAndEvaluate(trainingSet);
+        
         Encog.getInstance().shutdown();
+    }
+
+    private static void persistNetwork(BasicNetwork network, MLDataSet trainingSet){
+        double e = network.calculateError(trainingSet);
+        System.out.println("Network traiined to error: " + e);
+        System.out.println("Saving network");
+        try {
+            SerializeObject.save(new File(FILENAME), network);
+        } catch (IOException e1) {
+            System.out.println("Saving network FAILED!");
+            e1.printStackTrace();
+        }
+    }
+    public static void loadAndEvaluate(MLDataSet trainingSet) {
+        System.out.println("Loading network");
+        BasicNetwork network = null;
+        try {
+            network = (BasicNetwork) SerializeObject.load(new File(FILENAME));
+            double e = network.calculateError(trainingSet);
+            System.out.println("Loaded network's error is(should be same as above): " + e);
+        } catch (IOException e) {
+            System.out.println("Restoring network FAILED!");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Restoring network FAILED!");
+            e.printStackTrace();
+        }
     }
 
     private static void askNetwork(BasicNetwork network, MLDataPair queryData) {
